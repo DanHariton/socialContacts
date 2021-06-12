f1481e33303bd5000a533106d0658120c39480a8
Danylo Kharytonov <danilhariton@gmail.com>
2021-06-12 14:45:39 +0200

    install laravel mix webpack

---
+++ templates/_js/app.js
@@ -0,0 +1,5 @@
+// Vendors -------------------------------------------------------------------------------------------------------------
+window.$ = window.jQuery = require('../../node_modules/jquery/dist/jquery.slim.min');
+require('../../node_modules/bootstrap/dist/js/bootstrap.bundle.min');
+require('startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.js');
+require('startbootstrap-sb-admin-2/js/sb-admin-2');
